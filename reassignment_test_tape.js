b0cb628ac8a80ddf762527ba886084a0cead73f0
Michaël L <michaellaurac@gmail.com>
2022-07-04 22:17:56 +0200

    Added a few test examples using array iterative calls

--- reassignment_test_tape.js
+++ reassignment_test_tape.js
@@ -79,3 +79,23 @@ test("verifies that when numberX is 1 and numberY is 99 the function returns 99"
   assert.strictEqual(reassignmentWithoutNestingInConditional(1, 99), 99);
   assert.end();
 });
+
+// functional tests
+test("verifiers that the forEach function run on [3, 4, 2] yields [4, 5, 3]", (assert) => {
+  const forEachArray = [];
+  [3, 4, 2].forEach((element) => forEachArray.push(element + 1));
+  assert.deepEqual(forEachArray, [4, 5, 3]);
+  assert.end();
+});
+test("verifiers that the map function run on [3, 4, 2] yields [6, 8, 4]", (assert) => {
+  assert.deepEqual([3, 4, 2].map((element) => element * 2), [6, 8, 4]);
+  assert.end();
+});
+test("verifiers that the filter function run on [3, 4, 2] yields [4, 2]", (assert) => {
+  assert.deepEqual([3, 4, 2].filter((element) => element % 2 === 0), [4, 2]);
+  assert.end();
+});
+test("verifiers that the reduce function run on [3, 4, 2] yields 9", (assert) => {
+  assert.strictEqual([3, 4, 2].reduce((element, accumulator) => element + accumulator), 9);
+  assert.end();
+});
